38148688bd0b5196e047a5359032f2429400fe90
MoonlightingDev <arhysjacobson@gmail.com>
2024-10-21 07:42:30 +1100

    First Push

---
+++ File Organiser/File_Organiser.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+
+
+folder_path = "C:\Users\MoonlightingDev\Desktop\Test"
+
+audio = (".3ga", ".aac", ".ac3", ".aif", ".aiff",
+         ".alac", ".amr", ".ape", ".au", ".dss",
+         ".flac", ".flv", ".m4a", ".m4b", ".m4p",
+         ".mp3", ".mpga", ".ogg", ".oga", ".mogg",
+         ".opus", ".qcp", ".tta", ".voc", ".wav",
+         ".wma", ".wv")
+
+video = (".webm", ".MTS", ".M2TS", ".TS", ".mov",
+         ".mp4", ".m4p", ".m4v", ".mxf")
+
+img = (".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp", ".png",
+       ".gif", ".webp", ".svg", ".apng", ".avif", "heic", "")
+
+exe = ("exe")
+
+data = ("cvs", "exls", "xls")
+
+documents = ("txt", "doc", "docx", "pdf", "")
+
+unknown = ("7z", "zip", "")
+
+code = ("php", "py", "html", "json")
